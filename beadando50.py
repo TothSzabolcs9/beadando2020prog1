1082bba286847de8ab2c16d45281d3927131c6e7
TothSzabolcs9 <37191711+TothSzabolcs9@users.noreply.github.com>
2020-05-12 00:53:57 +0200

    Update beadando50.py

--- beadando50.py
+++ beadando50.py
@@ -2,20 +2,25 @@ n = int(input("Útszakasz hossza méterben: "))/1000
 m = int(input("Sebességkorlát: "))
 
 
-line = input("Bemenet: ")
+line = input("Adja meg az útszakaszra való belépés időpontját,kilépés időpontját és az autó rendszámát: ")
 
 data = []
 
-while line != "end":
-    data.append(line.split(" "))
-    line = input("Bemenet: ")
 
-for x in range(len(data)):
-    hr = int(data[x][3]) - int(data[x][0])
-    min = int(data[x][4]) - int(data[x][1])
-    sec = int(data[x][5]) - int(data[x][2])
+try:
+    while line != "end":
+        data.append(line.split(" "))
+        line = input("Adja meg az útszakaszra való belépés időpontját,kilépés időpontját és az autó rendszámát: ")
 
-    t = (hr*3600 + min*60 + sec)/3600
+    for x in range(len(data)):
+        hr = int(data[x][3]) - int(data[x][0])
+        min = int(data[x][4]) - int(data[x][1])
+        sec = int(data[x][5]) - int(data[x][2])
 
-    if n/t > m:
-      print(data[x][6], "{:.2f}km/h".format(n/t))
+        t = (hr*3600 + min*60 + sec)/3600
+
+        if n/t > m:
+            print(data[x][6], "{:.2f}km/h".format(n/t))
+
+except IndexError:
+    print("Nem jó formátumban adtad meg az értékeket!")
